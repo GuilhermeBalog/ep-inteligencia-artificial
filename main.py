fe16f9eb5f47c1c5560bb1a9661857bd2eb8b9c1
Laura <laurazsouza@yahoo.com.br>
2021-06-27 17:11:28 -0300

    adiciona comentários e arruma nome das imagens

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ from sklearn.metrics import plot_confusion_matrix
 classifiers = [KNeighborsClassifier(), MLPClassifier(), DecisionTreeClassifier()]
 params = [knn, mlp, decisionTree]
 
-np.set_printoptions(precision=3, suppress=True)
-pd.set_option('display.expand_frame_repr', False)
-
 search_count = 0
 
 def save(title):
@@ -46,7 +43,7 @@ def confusion_matrix(name, estimator, X, y):
 
     plt.figure()
     plot_confusion_matrix(estimator, X, y, cmap='Blues')
-    save(f'{name}-{search_count}')
+    save(f'{name}')
 
 
 def normalize(X):
@@ -70,16 +67,14 @@ def normalize(X):
     return X_novo
 
 
-def param_calibration(x, y):
-    search_count += 1
-
+def param_calibration(x, y, iteration):
     # Separa o conjunto de dados em 80% para treino e 20% para teste
     X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=0.8)
 
     # Define a estratégia de validação cruzada com 10 divisões
     cv = StratifiedKFold(n_splits=10)
 
-    # Itera sobre os classificadores e seus parâmetros
+    # Itera sobre os classificadores (knn, mlp e decisionTree) e seus parâmetros
     for classifier, param in zip(classifiers, params):
         print("\nClassficador e parametros: ")
         print(classifier, param)
@@ -89,6 +84,7 @@ def param_calibration(x, y):
         grid_search = GridSearchCV(
             estimator=classifier,
             param_grid=param,
+            # Define as métricas de melhor combinação de classificado/parâmetro
             scoring=['accuracy', 'precision', 'recall', 'f1'],
             n_jobs=-1,
             cv=cv,
@@ -112,7 +108,7 @@ def param_calibration(x, y):
         print("Melhores parametros: ", parameters)
 
         # Cria matriz de confusão
-        confusion_matrix(classifier, results.best_estimator_, X_test, y_test)
+        confusion_matrix(f'{classifier}-{iteration}', results.best_estimator_, X_test, y_test)
 
 
 
@@ -120,15 +116,14 @@ def show_dataset_info(x):
 
     x_info = x.describe()
 
-    print("Informações gerais sobre o conjunto de dados: ")
-    print(x_info)
-
+    # Obtém valores máximos de cada atributo
     x_max_values = np.sort(x_info.loc[['max']].values[0])
     print("Valores máximos por características:")
     print(x_max_values.tolist()[:28])
     print(x_max_values.tolist()[28:])
     print("\nMédia dos valores máximos: ", x_max_values.mean())
 
+    # Obtém média dos valores de cada atributo
     x_mean_values = np.sort(x_info.loc[['mean']].values[0])
     print("\nMédia dos valores por características:")
     print("%.2f " * len(x_mean_values[:28]) % tuple(x_mean_values[:28]))
@@ -137,6 +132,10 @@ def show_dataset_info(x):
 
 
 if __name__ == '__main__':
+    # Configurações de print
+    np.set_printoptions(precision=3, suppress=True)
+    pd.set_option('display.expand_frame_repr', False)
+
     # Carrega o conjunto de dados
     x = pd.read_csv('spambase.csv')
 
@@ -153,9 +152,12 @@ if __name__ == '__main__':
 
     print("----Informações gerais dos dados antes do processamento: -----")
     show_dataset_info(x)
-    param_calibration(x.values, y)
+    param_calibration(x.values, y, 1)
 
+    # Normaliza os dados
     normalized_x = normalize(x)
+    # Não foi nescessário fazer balanceamento nem lidar com dados faltantes
+
     print("\n----Informações gerais dos dados depois do processamento: ----")
     show_dataset_info(normalized_x)
-    param_calibration(normalized_x.values, y)
+    param_calibration(normalized_x.values, y, 2)
--- parameters.py
+++ parameters.py
@@ -1,14 +1,14 @@
 knn = {
-    "n_neighbors": [5, 4, 6, 7, 3], # pelo menos 5 valores
-    "metric": ["euclidean", "manhattan"],
+    "n_neighbors": [5, 4, 6, 7, 3], # Quantidade de vizinhos
+    "metric": ["euclidean", "manhattan"], # Tipo da fórmula de distância utilizada
 }
 
 mlp = {
-    "hidden_layer_sizes": [(6,), (7,), (8,), (9,)]
+    "hidden_layer_sizes": [(6,), (7,), (8,), (9,)] # Número de neurônios na camada escondida
 }
 
 decisionTree = {
     "criterion": ["gini", "entropy"],
-    "max_depth": [11, 10, 9, 8, 12],  # pelo menos 5 valores
-    "min_samples_leaf": [1, 3, 5, 7, 9] # pelo menos 5 valores
+    "max_depth": [11, 10, 9, 8, 12],
+    "min_samples_leaf": [1, 3, 5, 7, 9] # Número mínimo de amostras para ser uma folha
 }
